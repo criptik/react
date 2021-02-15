95ac5ef441dc73c591a160e6a70004a8c3c9d702
criptik <tom.deneau@gmail.com>
2021-02-15 16:01:34 -0600

    first checkpoint, saving state todo app

--- react-todo-app/src/components/TodoContainer.js
+++ react-todo-app/src/components/TodoContainer.js
@@ -5,6 +5,10 @@ import InputTodo from "./InputTodo";
 
 class TodoContainer extends React.Component {
     state = {
+        todos: [],
+    };
+    
+    dummyState = {
         todos: [
             {
                 id: 1,
@@ -25,6 +29,29 @@ class TodoContainer extends React.Component {
     };        
 
     nextid=4;
+
+    componentDidMount() {
+        const temp = localStorage.getItem("todos")
+        const loadedTodos = JSON.parse(temp)
+        if (loadedTodos) {
+            this.setState({
+                todos: loadedTodos
+            })
+        }
+    }
+
+    dummyComponentDidMount() {
+        fetch("https://jsonplaceholder.typicode.com/todos")
+            .then(response => response.json())
+            .then(data => console.log(data));
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        if(prevState.todos !== this.state.todos) {
+            const temp = JSON.stringify(this.state.todos)
+            localStorage.setItem("todos", temp)
+        }
+    }
 
     setUpdateTitle(updatedTitle, id) {
         console.log(updatedTitle, id);
