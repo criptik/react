bf43399f63accaa68f40a6f52cb4300174e46a91
Tom Deneau <tom.deneau@gmail.com>
2021-10-20 15:32:17 -0500

    script to create shared Projects dir

---
+++ createSharedProjectsDir.sh
@@ -0,0 +1,6 @@
+npx create-react-app Projects
+cd Projects
+# add various installs that one or more projects may need...
+npm install react-icons --save
+npm install react-router-dom --save
+npm install react-numeric-input --save
