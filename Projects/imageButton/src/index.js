ad84c9bb8ddd219e23c80e363a92141886bce0e3
Tom Deneau <tom.deneau@gmail.com>
2021-11-01 18:47:47 -0500

    imageButton sandbox using canvas.toDataURL

--- Projects/imageButton/src/index.js
+++ Projects/imageButton/src/index.js
@@ -5,12 +5,13 @@ const { createCanvas } = require("canvas");
 
 class MyButton extends React.Component {
     render() {
-        console.log('in MyButton.render,', this.props.imageUrl);
+        // console.log('in MyButton.render,', this.props.imageUrl);
         return (
             <button className="header_btn" style={(this.props.highlight) ? {borderWidth:5} : {borderWidth:1}} onClick={this.props.onClick} >
-                  <img height="60px" width="60px" alt="missing" src={this.props.imageUrl.src} />
+                  <img alt="missing" src={this.props.imageUrl} />
             </button>
         )
+        // <img height="60px" width="60px" alt="missing" src={this.props.imageUrl} />
     }
 }
 //               
@@ -22,24 +23,29 @@ class Base extends React.Component {
         super();
         this.state={};
         this.state.highlight = false;
+        this.state.coloridx = 0;
+        this.state.canvasURL = [];
+        this.state.colors = ['red', 'green', 'yellow'];
+
+        this.state.colors.forEach((color) => {
+            const canvas = createCanvas(60, 60);
+            const ctx = canvas.getContext("2d");
+            ctx.strokeStyle = 'blue';
+            ctx.lineWidth = 2;
+            ctx.fillStyle = color;
+            ctx.fillRect(10, 10, 26, 26);
+            ctx.strokeRect(4,4, 50, 50);
+            ctx.closePath();
+            let cidx = this.state.colors.indexOf(color);
+            this.state.canvasURL[cidx] = canvas.toDataURL();
+        });
     }
 
     render() {
-        const canvas = createCanvas(100, 200);
-        const ctx = canvas.getContext("2d");
-        ctx.lineWidth = 4;
-        ctx.strokeRect(0, 0, 30, 30);
-        ctx.fillStyle = 'red';
-        ctx.fillRect(0, 0, 25, 25);
-        ctx.closePath();
         
-        var target = new Image();
-        target.src = canvas.toDataURL();
-        console.log(target);
-
         return ( <MyButton
                    text=""
-                   imageUrl={target} //"image.png"
+                   imageUrl={this.state.canvasURL[this.state.coloridx]} //"image.png"
                    highlight={this.state.highlight}
                    onClick={()=>this.myclick()}
                  />
@@ -48,9 +54,12 @@ class Base extends React.Component {
     myclick() {
         console.log('Click!');
         let newhighlight = !this.state.highlight;
+        let newcoloridx = (this.state.coloridx + 1) % 3;
         this.setState({
             highlight: newhighlight,
+            coloridx: newcoloridx,
         });
+        
         
     }
 }
